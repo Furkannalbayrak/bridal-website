4504608c9933a26014a56d42507740392c155558
Furkan Albayrak <furkanalb42@gmail.com>
2025-08-01 22:10:44 +0300

    gelinlik kartları icerigi ve URL yonlendirme duzenlendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { FaBars, FaTimes, FaShoppingCart, FaUser, FaChevronDown, FaChevronUp } f
 import { Link } from 'react-router-dom';
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoMdClose } from "react-icons/io";
+import AllCollection from './AllCollection'
 
 const DressModelsDropdown = ({ isScrolled }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,60 +12,74 @@ const DressModelsDropdown = ({ isScrolled }) => {
 
   const dressCategories = [
     {
-      title: 'Etep Tipi',
+      title: 'Etek Tipi',
+      category: 'etek-tipi',
       models: [
-        { name: 'Prenses Gelinlik', path: '/gelinlik-modelleri/prenses' },
-        { name: 'A kesim Gelinlik', path: '/gelinlik-modelleri/A-kesim' },
-        { name: 'Helen Gelinlik', path: '/gelinlik-modelleri/helen' },
-        { name: 'Balık Gelinlik', path: '/gelinlik-modelleri/balik' },
-        { name: 'Kısa Gelinlik', path: '/gelinlik-modelleri/kisa' },
-        { name: 'Kabarık Gelinlik', path: '/gelinlik-modelleri/Kabarik' },
+        { name: 'Prenses Gelinlik', slug: 'prenses' },
+        { name: 'A kesim Gelinlik', slug: 'a-kesim' },
+        { name: 'Helen Gelinlik', slug: 'helen' },
+        { name: 'Balık Gelinlik', slug: 'balik' },
+        { name: 'Kısa Gelinlik', slug: 'kisa' },
+        { name: 'Kabarık Gelinlik', slug: 'kabarik' },
       ]
     },
     {
       title: 'Yaka Tipi',
+      category: 'yaka-tipi',
       models: [
-        { name: 'Kayık Yaka Gelinlik', path: '/gelinlik-modelleri/kayik-yaka' },
-        { name: 'Hakim Yaka Gelinlik', path: '/gelinlik-modelleri/hakim-yaka' },
-        { name: 'V Yaka Gelinlik', path: '/gelinlik-modelleri/V-yaka' },
-        { name: 'Kalp Yaka Gelinlik', path: '/gelinlik-modelleri/kalp-yaka' },
-        { name: 'Kare Yaka Gelinlik', path: '/gelinlik-modelleri/kare-yaka' },
+        { name: 'Kayık Yaka Gelinlik', slug: 'kayik-yaka' },
+        { name: 'Hakim Yaka Gelinlik', slug: 'hakim-yaka' },
+        { name: 'V Yaka Gelinlik', slug: 'v-yaka' },
+        { name: 'Kalp Yaka Gelinlik', slug: 'kalp-yaka' },
+        { name: 'Askılı Gelinlik', slug: 'askili' },
+        { name: 'Kare Yaka Gelinlik', slug: 'kare-yaka' },
+        { name: 'Omuz Açık Gelinlik', slug: 'omuz-acik' },
       ]
     },
     {
       title: 'Kol Tipi',
+      category: 'kol-tipi',
       models: [
-        { name: 'Uzun Kollu Gelinlik', path: '/gelinlik-modelleri/uzun-kollu' },
-        { name: 'Balon Kol Gelinlik', path: '/gelinlik-modelleri/balon-kol' },
-        { name: 'Tek Omuz Gelinlik', path: '/gelinlik-modelleri/tek-omuz' },
-        { name: 'Düşük Omuz Gelinlik', path: '/gelinlik-modelleri/dusuk-omuz' },
-        { name: 'Kolsun/Sıfır Kol Gelinlik', path: '/gelinlik-modelleri/sifir-kol' },
+        { name: 'Uzun Kollu Gelinlik', slug: 'uzun-kollu' },
+        { name: 'Yarım Kollu Gelinlik', slug: 'yarim-kollu' },
+        { name: 'Balon Kollu Gelinlik', slug: 'balon-kollu' },
+        { name: 'Tek Omuz Gelinlik', slug: 'tek-omuz' },
+        { name: 'Düşük Omuz Gelinlik', slug: 'dusuk-omuz' },
+        { name: 'Kolsuz/Sıfır Kollu Gelinlik', slug: 'kolsuz' },
       ]
     },
     {
       title: 'Kumaş',
+      category: 'kumas',
       models: [
-        { name: 'Dantelli Gelinlik', path: '/gelinlik-modelleri/ozel-kesim' },
-        { name: 'Tül Gelinlik', path: '/gelinlik-modelleri/el-isi' },
-        { name: 'Saten Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Fransız Dantelli Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Şifon Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Simli Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
+        { name: 'Dantelli Gelinlik', slug: 'dantelli' },
+        { name: 'Tül Gelinlik', slug: 'tul' },
+        { name: 'Saten Gelinlik', slug: 'saten' },
+        { name: 'Fransız Dantelli Gelinlik', slug: 'fransiz-dantelli' },
+        { name: 'Şifon Gelinlik', slug: 'sifon' },
+        { name: 'Simli Gelinlik', slug: 'simli' },
       ]
     },
     {
-      title: 'Tarzlar',
+      title: 'Tarzlar / Konseptler',
+      category: 'tarzlar',
       models: [
-        { name: 'Bohem Gelinlik', path: '/gelinlik-modelleri/ozel-kesim' },
-        { name: 'Vintage Gelinlik', path: '/gelinlik-modelleri/el-isi' },
-        { name: 'Sade Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Zarif Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Modern Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Sade Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-
+        { name: 'Bohem Gelinlik', slug: 'bohem' },
+        { name: 'Vintage Gelinlik', slug: 'vintage' },
+        { name: 'Sade Gelinlik', slug: 'sade' },
+        { name: 'Zarif Gelinlik', slug: 'zarif' },
+        { name: 'Modern Gelinlik', slug: 'modern' },
       ]
     }
   ];
+
+  const [selectedFilters, setSelectedFilters] = useState({
+    etekTipi: [],
+    yakaTipi: [],
+    kolTipi: [],
+    kumas: [],
+    konsept: []
+  });
 
 
   const handleMouseEnter = () => {
@@ -118,12 +133,14 @@ const DressModelsDropdown = ({ isScrolled }) => {
             <div className="grid grid-cols-5 gap-8">
               {dressCategories.map((category, index) => (
                 <div key={index} className="p-4">
-                  <h4 className="font-semibold text-lg text-rose-700 mb-4 pb-2 border-b border-gray-200">{category.title}</h4>
+                  <h4 className="font-semibold text-lg text-rose-700 mb-4 pb-2 border-b border-gray-200">
+                    {category.title}
+                  </h4>
                   <ul className="space-y-2">
                     {category.models.map((model, modelIndex) => (
                       <li key={modelIndex} className="mb-2">
                         <Link
-                          to={model.path}
+                          to={`/tum-gelinlik-modelleri/${category.category}/${model.slug}`}
                           className="block py-2 px-3 text-gray-600 hover:text-rose-600 hover:bg-rose-50 rounded-md transition-colors text-sm"
                           onClick={() => setIsOpen(false)}
                         >
@@ -137,7 +154,7 @@ const DressModelsDropdown = ({ isScrolled }) => {
             </div>
             <div className="mt-6 text-center">
               <Link
-                to="tum-gelinlik-modelleri"
+                to="/tum-gelinlik-modelleri"
                 className="inline-block py-3 px-8 bg-rose-500 text-white rounded-md hover:bg-rose-600 transition-colors font-medium"
                 onClick={() => setIsOpen(false)}
               >
@@ -161,61 +178,63 @@ const MobileDressModels = ({ onClose }) => {
 
   const dressCategories = [
     {
-      title: 'Etep Tipi',
+      title: 'Etek Tipi',
+      category: 'etek-tipi',
       models: [
-        { name: 'Prenses Gelinlik', path: '/gelinlik-modelleri/saten' },
-        { name: 'A kesim Gelinlik', path: '/gelinlik-modelleri/dantel' },
-        { name: 'Helen Gelinlik', path: '/gelinlik-modelleri/tul' },
-        { name: 'Balık Gelinlik', path: '/gelinlik-modelleri/tul' },
-        { name: 'Kısa Gelinlik', path: '/gelinlik-modelleri/tul' },
-        { name: 'Kabarık Gelinlik', path: '/gelinlik-modelleri/tul' },
+        { name: 'Prenses Gelinlik', slug: 'prenses' },
+        { name: 'A kesim Gelinlik', slug: 'a-kesim' },
+        { name: 'Helen Gelinlik', slug: 'helen' },
+        { name: 'Balık Gelinlik', slug: 'balik' },
+        { name: 'Kısa Gelinlik', slug: 'kisa' },
+        { name: 'Kabarık Gelinlik', slug: 'kabarik' },
       ]
     },
     {
       title: 'Yaka Tipi',
+      category: 'yaka-tipi',
       models: [
-        { name: 'Kayık Yaka Gelinlik', path: '/gelinlik-modelleri/uzun-kollu' },
-        { name: 'Hakim Yaka Gelinlik', path: '/gelinlik-modelleri/islemeli' },
-        { name: 'V Yaka Gelinlik', path: '/gelinlik-modelleri/prenses' },
-        { name: 'Kalp Yaka Gelinlik', path: '/gelinlik-modelleri/prenses' },
-        { name: 'Kare Yaka Gelinlik', path: '/gelinlik-modelleri/prenses' },
-        { name: 'V Yaka Gelinlik', path: '/gelinlik-modelleri/prenses' },
+        { name: 'Kayık Yaka Gelinlik', slug: 'kayik-yaka' },
+        { name: 'Hakim Yaka Gelinlik', slug: 'hakim-yaka' },
+        { name: 'V Yaka Gelinlik', slug: 'v-yaka' },
+        { name: 'Kalp Yaka Gelinlik', slug: 'kalp-yaka' },
+        { name: 'Askılı Gelinlik', slug: 'askili' },
+        { name: 'Kare Yaka Gelinlik', slug: 'kare-yaka' },
+        { name: 'Omuz Açık Gelinlik', slug: 'omuz-acik' },
       ]
     },
     {
       title: 'Kol Tipi',
+      category: 'kol-tipi',
       models: [
-        { name: 'Uzun Kollu Gelinlik', path: '/gelinlik-modelleri/ozel-kesim' },
-        { name: 'Balon Kol Gelinlik', path: '/gelinlik-modelleri/el-isi' },
-        { name: 'Tek Omuz Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Düşük Omuz Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Kolsun/Sıfır Kol Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Tek Omuz Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-
+        { name: 'Uzun Kollu Gelinlik', slug: 'uzun-kollu' },
+        { name: 'Yarım Kollu Gelinlik', slug: 'yarim-kollu' },
+        { name: 'Balon Kollu Gelinlik', slug: 'balon-kollu' },
+        { name: 'Tek Omuz Gelinlik', slug: 'tek-omuz' },
+        { name: 'Düşük Omuz Gelinlik', slug: 'dusuk-omuz' },
+        { name: 'Kolsuz/Sıfır Kollu Gelinlik', slug: 'kolsuz' },
       ]
     },
     {
       title: 'Kumaş',
+      category: 'kumas',
       models: [
-        { name: 'Dantelli Gelinlik', path: '/gelinlik-modelleri/ozel-kesim' },
-        { name: 'Tül Gelinlik', path: '/gelinlik-modelleri/el-isi' },
-        { name: 'Saten Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Fransız Dantelli Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Şifon Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Simli Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-
+        { name: 'Dantelli Gelinlik', slug: 'dantelli' },
+        { name: 'Tül Gelinlik', slug: 'tul' },
+        { name: 'Saten Gelinlik', slug: 'saten' },
+        { name: 'Fransız Dantelli Gelinlik', slug: 'fransiz-dantelli' },
+        { name: 'Şifon Gelinlik', slug: 'sifon' },
+        { name: 'Simli Gelinlik', slug: 'simli' },
       ]
     },
     {
-      title: 'Tarzlar',
+      title: 'Tarzlar / Konseptler',
+      category: 'tarzlar',
       models: [
-        { name: 'Bohem Gelinlik', path: '/gelinlik-modelleri/ozel-kesim' },
-        { name: 'Vintage Gelinlik', path: '/gelinlik-modelleri/el-isi' },
-        { name: 'Sade Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Zarif Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Modern Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-        { name: 'Sade Gelinlik', path: '/gelinlik-modelleri/sifir-beden' },
-
+        { name: 'Bohem Gelinlik', slug: 'bohem' },
+        { name: 'Vintage Gelinlik', slug: 'vintage' },
+        { name: 'Sade Gelinlik', slug: 'sade' },
+        { name: 'Zarif Gelinlik', slug: 'zarif' },
+        { name: 'Modern Gelinlik', slug: 'modern' },
       ]
     }
   ];
@@ -234,7 +253,7 @@ const MobileDressModels = ({ onClose }) => {
         <div className="w-full py-2 pl-4 pr-2 bg-white">
           {dressCategories.map((category, catIndex) => (
             <div key={catIndex} className="mb-2">
-              <div 
+              <div
                 className="flex transition duration-100 items-center justify-between py-2 px-2 text-gray-700 hover:bg-rose-50 font-medium cursor-pointer"
                 onClick={() => {
                   const newOpenCategories = [...openCategories];
@@ -243,18 +262,18 @@ const MobileDressModels = ({ onClose }) => {
                 }}
               >
                 <span>{category.title}</span>
-                {openCategories[catIndex] ? 
-                  <FaChevronUp className="text-xs" /> : 
+                {openCategories[catIndex] ?
+                  <FaChevronUp className="text-xs" /> :
                   <FaChevronDown className="text-xs" />
                 }
               </div>
-              
+
               {openCategories[catIndex] && (
                 <div className="ml-3 border-l-2 border-gray-100 pl-3 py-1">
                   {category.models.map((model, modelIndex) => (
                     <Link
                       key={modelIndex}
-                      to={model.path}
+                      to={`/tum-gelinlik-modelleri/${category.category}/${model.slug}`}
                       className="block py-2 px-2 text-gray-600 hover:text-rose-600 hover:bg-rose-50 rounded-md transition-colors text-sm"
                       onClick={() => onClose()}
                     >
@@ -265,7 +284,7 @@ const MobileDressModels = ({ onClose }) => {
               )}
             </div>
           ))}
-          
+
           <div className="mt-4 pt-3 border-t border-gray-100">
             <Link
               to="tum-gelinlik-modelleri"
@@ -351,7 +370,7 @@ const Header = () => {
               className="text-gray-700 hover:text-rose-300 focus:outline-none "
             >
               {<RxHamburgerMenu size={24} />}
-              
+
             </button>
           </div>
         </div>
@@ -360,18 +379,16 @@ const Header = () => {
         {isMenuOpen && (
           <>
             {/* Dark overlay with fade effect */}
-            <div 
-              className={`fixed inset-0 bg-black z-40 lg:hidden transition-opacity duration-300 ${
-                isOverlayVisible ? 'opacity-50' : 'opacity-0'
-              }`}
+            <div
+              className={`fixed inset-0 bg-black z-40 lg:hidden transition-opacity duration-300 ${isOverlayVisible ? 'opacity-50' : 'opacity-0'
+                }`}
               onClick={toggleMenu}
             />
-            
+
             {/* Mobile menu panel with slide-in effect */}
-            <div 
-              className={`fixed top-0 left-0 h-full w-96 bg-white z-50 shadow-2xl transform transition-transform duration-500 ease-in-out overflow-y-auto ${
-                isMenuVisible ? 'translate-x-0' : '-translate-x-full'
-              }`}
+            <div
+              className={`fixed top-0 left-0 h-full w-96 bg-white z-50 shadow-2xl transform transition-transform duration-500 ease-in-out overflow-y-auto ${isMenuVisible ? 'translate-x-0' : '-translate-x-full'
+                }`}
               style={{ zIndex: 50 }}
             >
               <div className="p-6">
@@ -379,7 +396,7 @@ const Header = () => {
                   {navLinks.map((link) => (
                     <div key={link.name} className="w-full border-b border-gray-200">
                       {link.name === 'Gelinlik Modelleri' ? (
-                        <MobileDressModels onClose={toggleMenu}/>
+                        <MobileDressModels onClose={toggleMenu} />
                       ) : (
                         <Link
                           to={link.path}
